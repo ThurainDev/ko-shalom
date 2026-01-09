5d85fbfb3cc5b2283fa3d19e9080164db450c29f
Thurain Dev <thuraindev@Thurains-MacBook-Pro.local>
2026-01-10 02:12:18 +0630

    6

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,20 @@ const Visit = require('./models/Visit')
 const app = express();
 
 app.use(morgan('dev'));
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://ko-shalom.vercel.app',
+  'https://ko-shalom-oywf.vercel.app'
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 app.use(express.json());
